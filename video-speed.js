689a9ba0c68554e5620a1623247e667f1fcc93d3
sdanielspq <scott.daniels@proquest.com>
2014-04-22 16:07:16 -0400

    Update video-speed.js
    Enclosed the plugin and prevented the plugin from running on iOS devices

--- video-speed.js
+++ video-speed.js
@@ -1,67 +1,69 @@
 // videojs-speed-plugin
+(function(videojs) {
+	videojs.plugin('speed', function(options) {
+		var player = this,
+			items = [],
+			selectedItem;
 
-videojs.plugin('speed', function(options) {
-    var player = this,
-        items = [],
-        selectedItem;
+		function changeSpeed(item) {
+			var opt = item.options();
+			item.selected(true);
 
-    function changeSpeed(item) {
-        var opt = item.options();
-        item.selected(true);
-        player.M.playbackRate = opt.rate; // FIXME player.M
+			player.tag.playbackRate = opt.rate;
 
-        var nodes = player.controlBar.speedButton.el().firstChild.children;
-        for (var i = 0, l = nodes.length; i < l; i++) {
-            if (nodes[i].className == 'vjs-current-speed') {
-                nodes[i].innerHTML = opt.label;
-            }
-        }
-    }
+			var nodes = player.controlBar.speedButton.el().firstChild.children;
+			for (var i = 0, l = nodes.length; i < l; i++) {
+				if (nodes[i].className == 'vjs-current-speed') {
+					nodes[i].innerHTML = opt.label;
+				}
+			}
+		}
 
-    videojs.SpeedMenuItem = videojs.MenuItem.extend({
-        init: function(player, options) {
-            videojs.MenuItem.call(this, player, options);
-            this.on('click', this.onClick);
-        }
-    });
-    videojs.SpeedMenuItem.prototype.onClick = function(e) {
-        for (var i = 0, l = items.length; i < l; i++) {
-            items[i].selected(false);
-        }
-        changeSpeed(this);
-    }
+		videojs.SpeedMenuItem = videojs.MenuItem.extend({
+			init: function(player, options) {
+				videojs.MenuItem.call(this, player, options);
+				this.on('click', this.onClick);
+			}
+		});
+		videojs.SpeedMenuItem.prototype.onClick = function(e) {
+			for (var i = 0, l = items.length; i < l; i++) {
+				items[i].selected(false);
+			}
+			changeSpeed(this);
+		}
 
-    videojs.SpeedButton = videojs.MenuButton.extend({
-        init: function(player, options){
-            videojs.MenuButton.call(this, player, options);
-        }
-    });
-    videojs.SpeedButton.prototype.createItems = function() {
-        var item;
-        options.forEach(function(opt) {
-            item = new videojs.SpeedMenuItem(player, { label: opt.text, rate: opt.rate });
-            if (opt.selected) {
-                selectedItem = item;
-            }
-            items.push(item);
-        });
-        return items;
-    }
+		videojs.SpeedButton = videojs.MenuButton.extend({
+			init: function(player, options){
+				videojs.MenuButton.call(this, player, options);
+			}
+		});
+		videojs.SpeedButton.prototype.createItems = function() {
+			var item;
+			options.forEach(function(opt) {
+				item = new videojs.SpeedMenuItem(player, { label: opt.text, rate: opt.rate });
+				if (opt.selected) {
+					selectedItem = item;
+				}
+				items.push(item);
+			});
+			return items;
+		}
 
-    player.ready(function() {
-        var button = new videojs.SpeedButton(player, {
-            el: videojs.Component.prototype.createEl(null, {
-                className: 'vjs-res-button vjs-menu-button vjs-control',
-                innerHTML: '<div class="vjs-control-content" style="font-size: 11px; line-height: 28px;"><span class="vjs-current-speed"></span></div>',
-                role: 'button'
-            })
-        });
-        player.controlBar.speedButton = player.controlBar.addChild(button);
-        changeSpeed(selectedItem);
-        
-        if (player.M.id.indexOf('html5') == -1) { // FIXME player.M
-            button.hide();
-        }
-    });
-});
+		player.ready(function() {
+			// only for HTML5 player, but not iOS because iOS uses native HTML5 video player
+			if (player.techName.toLowerCase().indexOf('html5') != -1 && vjs.IS_IOS === false) {
 
+				var button = new videojs.SpeedButton(player, {
+					el: videojs.Component.prototype.createEl(null, {
+						className: 'vjs-res-button vjs-menu-button vjs-control',
+						innerHTML: '<div class="vjs-control-content" style="font-size: 11px; line-height: 28px;"><span class="vjs-current-speed"></span></div>',
+						role: 'button'
+					})
+				});
+				player.controlBar.speedButton = player.controlBar.addChild(button);
+				player.defaultPlaybackRate = selectedItem.options().rate;
+				changeSpeed(selectedItem);
+			}
+		});
+	});
+})(window.videojs);
